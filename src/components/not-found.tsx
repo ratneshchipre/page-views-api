8dd2427ab934a4b6606c43ec49231943e8a04fe9
ratneshchipre <ratneshchipre@gmail.com>
2026-03-22 16:50:02 +0530

    feat: add GitHub stars component, icons, and not found page

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,3 +1,37 @@
-export default function NotFound() {
-  return <div>not-found</div>;
+"use client";
+
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { ArrowRight02Icon } from "@hugeicons/core-free-icons";
+import { cn } from "@/lib/utils";
+
+export default function NotFound({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "flex h-[calc(100svh-2rem)] flex-col items-center justify-center gap-4",
+        className
+      )}
+    >
+      <div className="flex flex-col items-center gap-1">
+        <h1 className="font-geist-pixel-square text-8xl">404</h1>
+        <p className="font-geist-sans text-lg font-medium">Page Not Found</p>
+      </div>
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "default" }),
+          "font-geist-sans transition-colors hover:bg-primary/80! active:scale-98"
+        )}
+      >
+        Go to Home
+        <HugeiconsIcon
+          icon={ArrowRight02Icon}
+          strokeWidth={2}
+          className="size-4"
+        />
+      </Link>
+    </div>
+  );
 }
