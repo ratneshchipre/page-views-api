5632c39b00b458ab1022530d18cb3ee54a731a00
ratneshchipre <ratneshchipre@gmail.com>
2026-03-24 18:55:45 +0530

    feat: implement code block components, enhance documentation page, and add new UI elements

--- src/app/(app)/(docs)/docs/[slug]/page.tsx
+++ src/app/(app)/(docs)/docs/[slug]/page.tsx
@@ -1,3 +1,43 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+import { getAllPosts } from "@/data/docs";
+import { MDX } from "@/components/mdx";
+import { Prose } from "@/components/ui/typography";
+
+export async function generateStaticParams() {
+  const posts = getAllPosts();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const slug = (await params).slug;
+  const post = getAllPosts().find((post) => post.slug === slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const { title, description, image, createdAt, updatedAt } = post.metadata;
+
+  const ogImage = image || "/images/opengraph-image.png";
+
+  return {
+    title,
+    description,
+    // image: ogImage,
+    // type: "article",
+    // publishedTime: toIsoDate(createdAt),
+    // modifiedTime: toIsoDate(updatedAt),
+  };
+}
+
 export default async function DocsPage({
   params,
 }: {
@@ -6,6 +46,32 @@ export default async function DocsPage({
   }>;
 }) {
   const slug = (await params).slug;
+  const post = getAllPosts().find((post) => post.slug === slug);
 
-  return <div>DocsPage</div>;
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <Prose className="font-geist-sans">
+        <div>
+          <MDX code={post.content} />
+        </div>
+      </Prose>
+    </div>
+  );
+}
+
+function toIsoDate(value?: string) {
+  if (!value) {
+    return undefined;
+  }
+
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) {
+    return undefined;
+  }
+
+  return date.toISOString();
 }
---
+++ src/components/code-block-command.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import * as React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import type { PackageManager } from "@/hooks/use-package-manager";
+import { usePackageManager } from "@/hooks/use-package-manager";
+import type { NpmCommands } from "@/types/unist";
+
+import CopyButton from "./copy-button";
+import { getIconForPackageManager } from "./icons";
+
+export function CodeBlockCommand({
+  __pnpm__,
+  __yarn__,
+  __npm__,
+  __bun__,
+}: NpmCommands) {
+  const [packageManager, setPackageManager] = usePackageManager();
+
+  const tabs = React.useMemo(() => {
+    return {
+      pnpm: __pnpm__,
+      yarn: __yarn__,
+      npm: __npm__,
+      bun: __bun__,
+    };
+  }, [__pnpm__, __yarn__, __npm__, __bun__]);
+
+  return (
+    <div className="bg-code relative overflow-hidden rounded-xl">
+      <Tabs
+        className="gap-0"
+        value={packageManager}
+        onValueChange={(value) => {
+          setPackageManager(value as PackageManager);
+        }}
+      >
+        <div className="px-4 shadow-[inset_0_-1px_0_0] shadow-border">
+          <TabsList className="h-10 rounded-none bg-transparent p-0 dark:bg-transparent [&_svg]:me-2 [&_svg]:size-4 [&_svg]:text-muted-foreground">
+            {getIconForPackageManager(packageManager)}
+
+            {Object.entries(tabs).map(([key]) => {
+              return (
+                <TabsTrigger
+                  key={key}
+                  className="h-7 rounded-lg p-0 px-2 font-mono"
+                  value={key}
+                >
+                  {key}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+        </div>
+        {Object.entries(tabs).map(([key, value]) => {
+          return (
+            <TabsContent key={key} value={key}>
+              <pre className="scroll-fade-effect-x">
+                <code
+                  data-slot="code-block"
+                  data-language="bash"
+                  className="font-mono text-sm leading-none text-muted-foreground"
+                >
+                  <span className="select-none">$ </span>
+                  {value}
+                </code>
+              </pre>
+            </TabsContent>
+          );
+        })}
+      </Tabs>
+      <CopyButton
+        className="absolute top-2 right-2 z-10 rounded-md border-none"
+        size="icon-xs"
+        value={tabs[packageManager] || ""}
+        event="copy_npm_command"
+      />
+    </div>
+  );
+}
